777b791b1d5fec75575388a5fa8fcde5f5063364
dsantosp12 <dsantosp12@gmail.com>
2016-09-30 16:40:25 -0400

    added db ORM

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+import config
 
 
 app = Flask(__name__)
+app.config.from_object(config)
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
